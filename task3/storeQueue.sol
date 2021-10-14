615f083282b8ac649c08a62509adb06f7296c6fd
chezetti <riza25@bk.ru>
2021-10-14 22:39:58 +0300

    added task 3

---
+++ task3/storeQueue.sol
@@ -0,0 +1,43 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+contract storeQueue {
+
+    string[] private queue;
+
+    uint8 ARRAY_IS_EMPTY = 103;
+
+    constructor() public {
+        require(tvm.pubkey() != 0, 101);
+        require(msg.pubkey() == tvm.pubkey(), 102);
+
+        tvm.accept();
+
+        queue.push("Billy");
+        queue.push("Jonathan");
+        queue.push("Bucky");
+        queue.push("Steve");
+
+    }
+
+    function addToArray(string name) public returns (string[]) {
+        tvm.accept();
+        queue.push(name);
+        return queue;
+    }
+
+    function deleteFromArray() public returns (string[]){
+        tvm.accept();
+        require(queue.empty() == false, ARRAY_IS_EMPTY);
+         for (uint i = 0; i<queue.length-1; i++) {
+            queue[i] = queue[i+1];
+        }
+        delete queue[queue.length-1];
+        queue.pop();
+        return queue;
+    }
+
+    function getQueue() public view returns (string[]) {
+        return queue;
+    }
+}
