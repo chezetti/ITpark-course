7a84f4f21130dab43be1ed391072abecf538493b
chezetti <riza25@bk.ru>
2021-10-14 22:40:35 +0300

    upd task 2

---
+++ task2/calculations.sol
@@ -0,0 +1,35 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+contract Multiplication {
+
+	uint private result = 1;
+	
+	uint8 INVALID_NUMBER = 103;
+
+	constructor() public {
+		require(tvm.pubkey() != 0, 101);
+		require(msg.pubkey() == tvm.pubkey(), 102);
+		tvm.accept();
+	}
+
+	modifier checkOwnerAndAccept {
+		require(msg.pubkey() == tvm.pubkey(), 102);
+		tvm.accept();
+		_;
+	}
+
+	function multiply(uint value) public checkOwnerAndAccept {
+		require(value >= 1 && value <= 10, INVALID_NUMBER); 
+			result = result*value;
+	}
+
+	function getResult() public view returns (uint) {
+		return result;
+	}
+
+	function setResult(uint _result) public {
+		tvm.accept();
+		result = _result;
+	}
+}
